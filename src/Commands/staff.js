b3c78630479356754ea97f741fdfcfdd2ad6bd7b
Fabio Krakauer <comvocefabio@gmail.com>
2020-08-19 13:32:21 -0300

    add embeed message to call staff comment

--- src/Commands/staff.js
+++ src/Commands/staff.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const utils = require('../utils');
 
 require('dotenv').config();
@@ -10,8 +11,22 @@ module.exports = (client, args, sender) => {
     for(let i = 2; i < args.length; i++){
         message += args[i] + " ";
     }
+    const messageEmbeed = new Discord.MessageEmbed()
+    .setColor('#FF5100')
+    .setTitle('CHAMADO STAFF')
+    .setThumbnail(client.user.displayAvatarURL())
+    .setAuthor(sender.member.user.username + "#"+sender.member.user.discriminator, sender.member.user.avatarURL())
+    .addFields(
+        { name: "Chamado enviado por", value: sender.member.nickname},
+        { name: '\u200B', value: '\u200B' },
+        { name: "Mensagem", value: message}
+    )
+    .setTimestamp()
+    .setFooter('Chamado de STAFF Copyright TOP Linhas Aéreas');
+
     client.channels.fetch('709211296092127390').then(channel => {
-        channel.send('@here \n' + '**CHAMADO STAFF**' + ' \n De ' + sender.member.user.username + "#"+sender.member.user.discriminator+ '\n- _' + message + '_')
+        channel.send("@here Novo chamado de STAFF")
+        channel.send(messageEmbeed);
     });
     sender.author.send("Sua mensagem foi enviada! Aguarde até 72h para uma resposta, caso não respondam envie de novo.");
 }
